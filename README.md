75dbd73601eb94b8fdf86443a868d6361946d354
will-e-yams <brad.lee.williams@gmail.com>
2014-03-26 01:56:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# yams.whichway.js
+###### a jQuery plugin that adds orientation classes to the body tag
+
+---
+
+On mobile devices, this will add the class `device-portrait` when the device is oriented portait and `device-landscape` when the device is oriented landscape.
+
+### Requires
+
+- [jQuery](#http://jQuery.com)
+
+### Usage
+
+```
+<script type="text/javascript" src="yams.whichway.js">
+
+```
---
+++ yams.whichway.js
@@ -0,0 +1,52 @@
+﻿
+/* ========================================================================
+* yams.whichway.js
+* version:	1.0
+* author:	Brad Williams <brad.lee.williams@gmail.com>
+* ======================================================================== */
+
+if (!$.yams) { $.yams = new Object(); }
+if (!$.yams.whichway) { $.yams.whichway = new Object(); }
+
+(function ($, $ww) {
+
+    $ww.init = function () {
+        window.addEventListener("orientationchange", function () {
+            $ww.orient();
+        });
+    }
+
+    $ww.orient = function () {
+        var orientation = $ww._getOrientation();
+        switch (orientation) {
+            case 'default':
+            case 'portrait':
+                $('body').addClass("device-portrait");
+                $('body').removeClass("device-landscape");
+                break;
+            case 'landscape':
+                $('body').addClass("device-landscape");
+                $('body').removeClass("device-portrait");
+                break;
+        }
+    };
+
+    $ww._getOrientation = function () {
+        var orientation = 'default';
+        switch (Math.abs(window.orientation * 1)) {
+            default:
+            case 0:
+            case 180:
+                orientation = "portrait";
+                break;
+            case 90:
+            case 270:
+                orientation = "landscape";
+                break;
+        }
+        return orientation;
+    }
+
+})(jQuery, $.yams.whichway);
+
+$.yams.whichway.init();
