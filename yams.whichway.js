948a860782fa0bb2e5e66e5fe9efbc89cecf0c9c
williamsb <williamsb@tutor.com>
2015-01-06 11:58:58 -0500

    new template; add callbacks

--- yams.whichway.js
+++ yams.whichway.js
@@ -1,52 +1,81 @@
 ﻿
 /* ========================================================================
 * yams.whichway.js
-* version:	1.0
+* version:	1.1
 * author:	Brad Williams <brad.lee.williams@gmail.com>
 * ======================================================================== */
 
 if (!$.yams) { $.yams = new Object(); }
-if (!$.yams.whichway) { $.yams.whichway = new Object(); }
 
-(function ($, $ww) {
+$.yams.whichway = (function () {
+    //private
 
-    $ww.init = function () {
-        window.addEventListener("orientationchange", function () {
-            $ww.orient();
-        });
-    }
+    var _callbacks = []
+    var _lastOrientation = {}
 
-    $ww.orient = function () {
-        var orientation = $ww._getOrientation();
-        switch (orientation) {
-            case 'default':
-            case 'portrait':
-                $('body').addClass("device-portrait");
-                $('body').removeClass("device-landscape");
-                break;
-            case 'landscape':
-                $('body').addClass("device-landscape");
-                $('body').removeClass("device-portrait");
-                break;
-        }
-    };
-
-    $ww._getOrientation = function () {
-        var orientation = 'default';
+    var _getOrientation = function () {
+        var orientation = 'default'
         switch (Math.abs(window.orientation * 1)) {
             default:
             case 0:
             case 180:
-                orientation = "portrait";
-                break;
+                orientation = "portrait"
+                break
             case 90:
             case 270:
-                orientation = "landscape";
-                break;
+                orientation = "landscape"
+                break
         }
         return orientation;
+    }
+    var _update = function() {
+        var orientation = _getOrientation()
+
+        if (!_lastOrientation
+            || _lastOrientation != orientation) {
+
+            // add class
+            switch (orientation) {
+                case 'default':
+                case 'portrait':
+                    $('body')
+                        .addClass("device-portrait")
+                        .removeClass("device-landscape")
+                    break
+                case 'landscape':
+                    $('body')
+                        .addClass("device-landscape")
+                        .removeClass("device-portrait")
+                    break
+            }
+
+            // run callback
+            if (defaults.enableResizeTracking) {
+                for (var i = 0; i < _callbacks.length; i++) {
+                    _callbacks[i](orientation)
+                }
+            }
+        }
+        _lastOrientation = orientation
+    }
+
+    //public
+    return {
+        addCallback: function (callback) {
+            _callbacks.push(callback)
+        },
+        init: function () {
+            window.addEventListener("orientationchange", function () {
+                orient()
+            });
+        },
+        getOrientation: function () {
+            return _lastOrientation
+        },
     }
 
 })(jQuery, $.yams.whichway);
 
-$.yams.whichway.init();
+$(function () { 
+    $.yams.whichway.init()
+})
